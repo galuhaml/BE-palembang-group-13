7ec4866ad232cc1a0bd402a619416394c3f390d1
galuhaml <142810971+galuhaml@users.noreply.github.com>
2023-10-04 21:05:21 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express"
+const server = express()
+const port = 3000
+
+server.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+server.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
